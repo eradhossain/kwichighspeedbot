7b73ddd6caaf486ab5c7be9c1066882787f8e882
eradhossain <98438334+eradhossain@users.noreply.github.com>
2022-12-30 15:53:39 +0300

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -134,7 +134,7 @@ async def channel_receive_handler(bot, broadcast):
     if MY_PASS:
         check_pass = await pass_db.get_user_pass(broadcast.chat.id)
         if check_pass == None:
-            await broadcast.reply_text("Login first using /login cmd \n don\'t know the pass? request it from @EH_UnknownPerson)
+            await broadcast.reply_text("Login first using /login cmd \n don\'t know the pass? request it from @kwicadmin)
             return
         if check_pass != MY_PASS:
             await broadcast.reply_text("Wrong password, login again")
